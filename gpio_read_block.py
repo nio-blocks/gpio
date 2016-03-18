7e5a04efc7d2ff84976c23bd3710752ea526e712
Chris Brackert <cbrackert@gmail.com>
2016-03-17 17:05:51 -0700

    Implement GPIORead block

--- gpio_read_block.py
+++ gpio_read_block.py
@@ -1,14 +1,67 @@
 from nio.block.base import Block
 from nio.util.discovery import discoverable
-from nio.properties import VersionProperty
+from nio.properties import BoolProperty, VersionProperty
+
+
+class GPIODevice():
+
+    """Communicate with a device over GPIO."""
+
+    def __init__(self, logger):
+        import RPi.GPIO as GPIO
+        self.logger = logger
+        GPIO.setmode(GPIO.BCM)
+        self._gpio_lock = Lock()
+
+    def read(self, pin):
+        """Read bool value from a pin.
+
+        Args:
+            pin (int): the pin to read from
+
+        Return:
+            bool: value of digital pin reading
+
+        """
+        with self._gpio_lock:
+            GPIO.setup(pin, GPIO.IN) # TODO: don't call this every time
+            value = GPIO.input(pin)
+            self.logger.debug("Value of GPIO pin {}: {}".format(pint, value))
+        return value
+
+    def close(self):
+        try:
+            GPIO.cleanup()
+        except:
+            self.logger.warning("Failed to close GPIO", exc_info=True)
 
 
 @discoverable
 class GPIORead(Block):
 
+    pin = BoolProperty(default=0)
     version = VersionProperty('0.1.0')
+
+    def __init__(self):
+        super().__init__()
+        self._gpio = None
+
+    def configure(self, context):
+        super().configure(context)
+        self._gpio = GPIODevice(self.logger)
+
+    def stop(self):
+        super().stop()
+        self._gpio.close()
 
     def process_signals(self, signals):
         for signal in signals:
-            pass
+            signal.value = self._read_gpio_pin(self.pin(signal))
         self.notify_signals(signals)
+
+    def _read_gpio_pin(self, pin):
+        try:
+            return self._gpio.read(pin)
+        except:
+            self.logger.warning("Failed to read gpio pin: {}".format(pin),
+                                exc_info=True)
--- tests/test_gpio_read_block.py
+++ tests/test_gpio_read_block.py
@@ -1,19 +1,40 @@
+from unittest import skip
+from unittest.mock import MagicMock, patch
 from nio.block.terminals import DEFAULT_TERMINAL
 from nio.signal.base import Signal
 from nio.testing.block_test_case import NIOBlockTestCase
-from ..gpio_read_block import GPIORead
+from ..gpio_read_block import GPIORead, GPIODevice
 
 
 class TestGPIORead(NIOBlockTestCase):
 
-    def test_process_signals(self):
+    @patch(GPIODevice.__module__ + ".GPIODevice", spec=GPIODevice)
+    def test_process_signals(self, mock_gpio):
         """Signals pass through block unmodified."""
         blk = GPIORead()
         self.configure_block(blk, {})
+        blk._gpio.read.return_value = True
         blk.start()
-        blk.process_signals([Signal({"hello": "n.io"})])
+        blk.process_signals([Signal()])
+        blk.stop()
+        blk._gpio.read.assert_called_once_with(0)
+        self.assert_num_signals_notified(1)
+        self.assertDictEqual(
+            self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
+            {"value": True})
+
+    @skip("Only run on a raspbeery pi")
+    def test_raspberry_pi(self):
+        """Raspberry pi uses RPi.GPIO library."""
+        blk = GPIORead()
+        self.configure_block(blk, {})
+        blk.start()
+        with patch('GPIO') as mock_gpio:
+            mock_gpio.input.return_value = True
+            blk.process_signals([Signal()])
         blk.stop()
         self.assert_num_signals_notified(1)
         self.assertDictEqual(
             self.last_notified[DEFAULT_TERMINAL][0].to_dict(),
-            {"hello": "n.io"})
+            {"value": True})
+        mock_gpio.input.assert_called_once_with(0)
